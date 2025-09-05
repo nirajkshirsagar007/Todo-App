a01c99f807d8a80bf1d72e50f6d823e67d9ce50c
nirajkshirsagar007 <nirajkshirsagar007@gmail.com>
2025-09-05 17:46:35 +0530

    updated controller- added code to update todo list if task is completed

--- src/main/java/com/nirajkshirsagar/todoapp/controller/TodoController.java
+++ src/main/java/com/nirajkshirsagar/todoapp/controller/TodoController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,6 +31,15 @@ public class TodoController {
                 .completed(false)
                 .build();
         todoRepository.save(newTodo);
+        return "redirect:/";
+    }
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id)
+    {
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Todo not found: "+id));
+        existingTodo.setCompleted(true);
+        todoRepository.save(existingTodo);
         return "redirect:/";
     }
 }
