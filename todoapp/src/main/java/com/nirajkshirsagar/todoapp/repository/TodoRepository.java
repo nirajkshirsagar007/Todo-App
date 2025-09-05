cd059588644457b48bc86491bacede774a761e4e
nirajkshirsagar007 <nirajkshirsagar007@gmail.com>
2025-09-05 18:18:03 +0530

    Deleted Unnecessory folder which i have created by mistake

--- todoapp/src/main/java/com/nirajkshirsagar/todoapp/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nirajkshirsagar.todoapp.repository;
-
-import com.nirajkshirsagar.todoapp.entity.TodoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-}
