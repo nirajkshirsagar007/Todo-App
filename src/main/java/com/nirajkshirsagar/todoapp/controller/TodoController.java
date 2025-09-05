91df53bb3c21faa514185603083d72e8268eef06
nirajkshirsagar007 <nirajkshirsagar007@gmail.com>
2025-09-05 17:10:01 +0530

    updated todoController- added postMapping

--- src/main/java/com/nirajkshirsagar/todoapp/controller/TodoController.java
+++ src/main/java/com/nirajkshirsagar/todoapp/controller/TodoController.java
@@ -1,10 +1,13 @@
 package com.nirajkshirsagar.todoapp.controller;
 
+import com.nirajkshirsagar.todoapp.entity.TodoEntity;
 import com.nirajkshirsagar.todoapp.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,9 +16,20 @@ public class TodoController {
     private final TodoRepository todoRepository;
 
     @GetMapping({"","/","/home"})
-    public String showHomePage(Model model){
+    public String showHomePage(Model model)
+    {
 
         model.addAttribute("todos",todoRepository.findAll());
         return "index";
+    }
+    @PostMapping("/add")
+    public String add(@RequestParam String title)
+    {
+        TodoEntity newTodo = TodoEntity.builder()
+                .title(title)
+                .completed(false)
+                .build();
+        todoRepository.save(newTodo);
+        return "redirect:/";
     }
 }
